bc82359283b3de8259644014d9a471e9c4922719
dave <dpadrino@hiberus.com>
2020-03-15 19:52:32 +0100

    Update ModalComponent to hooks

--- src/components/carouselComponent/index.js
+++ src/components/carouselComponent/index.js
@@ -1,70 +1,67 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Button from "@material-ui/core/Button";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const styles = theme => ({
   root: {
     maxWidth: 400,
-    flexGrow: 1,
+    flexGrow: 1
   },
   header: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     height: 50,
     paddingLeft: theme.spacing.unit * 4,
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: theme.palette.background.default
   },
   img: {
     height: 255,
-    display: 'block',
+    display: "block",
     maxWidth: 400,
-    overflow: 'hidden',
-    width: '100%',
-  },
+    overflow: "hidden",
+    width: "100%"
+  }
 });
 
-
-
 class SwipeableTextMobileStepper extends React.Component {
-    state = {
-        activeStep: 0,
-    };
-    
-    handleNext = () => {
-        this.setState(prevState => ({
-            activeStep: prevState.activeStep + 1,
-        }));
-    };
+  state = {
+    activeStep: 0
+  };
 
-    handleBack = () => {
-        this.setState(prevState => ({
-        activeStep: prevState.activeStep - 1,
-        }));
-    };
+  handleNext = () => {
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep + 1
+    }));
+  };
 
-    handleStepChange = activeStep => {
-        this.setState({ activeStep });
-    };
+  handleBack = () => {
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep - 1
+    }));
+  };
 
+  handleStepChange = activeStep => {
+    this.setState({ activeStep });
+  };
 
   render() {
+    console.log("this.props", this.props);
     const { classes, theme } = this.props;
     const { activeStep } = this.state;
     const maxSteps = this.props.tutorialSteps.length;
 
     return (
       <div className={classes.root}>
-          <AutoPlaySwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        <AutoPlaySwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
           index={activeStep}
           onChangeIndex={this.handleStepChange}
           enableMouseEvents
@@ -83,14 +80,30 @@ class SwipeableTextMobileStepper extends React.Component {
           activeStep={activeStep}
           className={classes.mobileStepper}
           nextButton={
-            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
+            <Button
+              size="small"
+              onClick={this.handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
               Next
-              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
             </Button>
           }
           backButton={
-            <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
-              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            <Button
+              size="small"
+              onClick={this.handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
               Back
             </Button>
           }
@@ -102,7 +115,9 @@ class SwipeableTextMobileStepper extends React.Component {
 
 SwipeableTextMobileStepper.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(SwipeableTextMobileStepper);
+export default withStyles(styles, { withTheme: true })(
+  SwipeableTextMobileStepper
+);
--- src/components/modalComponent/index.js
+++ src/components/modalComponent/index.js
@@ -1,88 +1,59 @@
-import React from 'react';
-import Modal from '@material-ui/core/Modal';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Modal from "@material-ui/core/Modal";
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
 
-import SwipeableTextMobileStepper from '../carouselComponent';
+import SwipeableTextMobileStepper from "../carouselComponent";
 
-import '../../styles/style.scss';
+const Paper = styled.div`
+  width: 90%;
+  transform: translate(5%, 50px);
+  background-color: #fff;
+  box-shadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)";
+  padding: 15px;
+  outline: "none";
+`;
 
-const styles = theme => ({
-    paper: {
-        width: '90%',
-        transform: 'translate(5%, 50px)',
-        backgroundColor: theme.palette.background.paper,
-        boxShadow: theme.shadows[5],
-        padding: '15px',
-        outline: 'none',
-  },
-});
-
-
-class SimpleModal extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            project: null,
-            images: []
-        }
-      }
-
-    componentDidMount = () => {
-       if (this.props.project) {
-           this.setState({project: this.props.project});
-       }                    
-    }
-    
-    render() {
-        return (
-            <div>
-            {this.state.project &&
-                <Modal
-                    aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-                open={this.props.open || false}
-                onClose={this.props.handleClose}
-                >
-                    <div className={this.props.classes.paper}>
-                        <Typography variant="h4" id="modal-title" className="modal-header">
-                            <button type="button" 
-                                className="close"
-                                onClick={this.props.handleClose}>
-                                &times;
-                            </button>
-                            {this.state.project.projectName}
-                        </Typography>
-                        <div className="modal-body">
-                            <div className="row">
-                                <div className="carousel slide col-xs-12 col-md-7">
-                                    <SwipeableTextMobileStepper tutorialSteps={this.state.project.images}/>
-                                </div>
-                                <div className="col-md-5 col-xs-12">
-                                <h4>Description</h4>
-                                <p>
-                                    {this.state.project.description}
-                                </p>
-                                <h4>Technologies</h4>
-                                <ul>
-                                    {this.state.project.technologies.split(',')
-                                        .map(el => <li key={el}>{el}</li>)}
-                                </ul>
-                                <h4>Role</h4>
-                                {this.state.project.role}
-                                </div>
-                            </div>
-                        </div>
-                        <SimpleModalWrapped />
-                    </div>
-                </Modal>
-            }
+const SimpleModal = ({ project, open, handleClose }) => (
+  <React.Fragment>
+    {project && (
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open || false}
+        onClose={handleClose}
+      >
+        <Paper>
+          <Typography variant="h4" id="modal-title" className="modal-header">
+            <button type="button" className="close" onClick={handleClose}>
+              &times;
+            </button>
+            {project.projectName}
+          </Typography>
+          <div className="modal-body">
+            <div className="row">
+              <div className="carousel slide col-xs-12 col-md-7">
+                <SwipeableTextMobileStepper tutorialSteps={project.images} />
+              </div>
+              <div className="col-md-5 col-xs-12">
+                <h4>Description</h4>
+                <p>{project.description}</p>
+                <h4>Technologies</h4>
+                <ul>
+                  {project.technologies.split(",").map(el => (
+                    <li key={el}>{el}</li>
+                  ))}
+                </ul>
+                <h4>Role</h4>
+                {project.role}
+              </div>
             </div>
-        );
-  }
-}
+          </div>
+          <SimpleModal />
+        </Paper>
+      </Modal>
+    )}
+  </React.Fragment>
+);
 
-
-const SimpleModalWrapped = withStyles(styles)(SimpleModal);
-
-export default SimpleModalWrapped;
+export default SimpleModal;
