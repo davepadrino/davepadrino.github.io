1267b64aa8cabf3908578b3af4cea451bf89ca97
dave <dpadrino@hiberus.com>
2020-03-29 17:59:51 +0200

    Change modal styles to styled

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,9 +9,48 @@ const Paper = styled.div`
   width: 90%;
   transform: translate(5%, 50px);
   background-color: #fff;
-  box-shadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)";
+  box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12);
   padding: 15px;
-  outline: "none";
+  outline: none;
+`;
+
+const ModalTitle = styled(Typography)`
+  padding: 15px;
+  border-bottom: 1px solid #e5e5e5;
+  & > button {
+    -webkit-appearance: none;
+    padding: 0;
+    cursor: pointer;
+    background: 0 0;
+    border: 0;
+    outline: none;
+    float: right;
+    line-height: 1;
+    color: #000;
+    opacity: 0.2;
+  }
+  & > button:hover {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+    opacity: 0.5;
+  }
+`;
+
+const ModalBody = styled.div`
+  position: relative;
+  padding: 15px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ProjectSummary = styled.div`
+  flex-grow: 1;
+`;
+
+const CarouselSlide = styled(ProjectSummary)`
+  position: relative;
 `;
 
 const SimpleModal = ({ project, open, handleClose }) => (
@@ -24,31 +63,29 @@ const SimpleModal = ({ project, open, handleClose }) => (
         onClose={handleClose}
       >
         <Paper>
-          <Typography variant="h4" id="modal-title" className="modal-header">
-            <button type="button" className="close" onClick={handleClose}>
+          <ModalTitle variant="h4" id="modal-title">
+            <button type="button" onClick={handleClose}>
               &times;
             </button>
             {project.projectName}
-          </Typography>
-          <div className="modal-body">
-            <div className="row">
-              <div className="carousel slide col-xs-12 col-md-7">
-                <SwipeableTextMobileStepper tutorialSteps={project.images} />
-              </div>
-              <div className="col-md-5 col-xs-12">
-                <h4>Description</h4>
-                <p>{project.description}</p>
-                <h4>Technologies</h4>
-                <ul>
-                  {project.technologies.split(",").map(el => (
-                    <li key={el}>{el}</li>
-                  ))}
-                </ul>
-                <h4>Role</h4>
-                {project.role}
-              </div>
-            </div>
-          </div>
+          </ModalTitle>
+          <ModalBody>
+            <CarouselSlide>
+              <SwipeableTextMobileStepper tutorialSteps={project.images} />
+            </CarouselSlide>
+            <ProjectSummary>
+              <h4>Description</h4>
+              <p>{project.description}</p>
+              <h4>Technologies</h4>
+              <ul>
+                {project.technologies.split(",").map(el => (
+                  <li key={el}>{el}</li>
+                ))}
+              </ul>
+              <h4>Role</h4>
+              {project.role}
+            </ProjectSummary>
+          </ModalBody>
           <SimpleModal />
         </Paper>
       </Modal>
