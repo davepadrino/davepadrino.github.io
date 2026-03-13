3a8fa57f0b01190d5c90aec4f14c8a7f12fcbdfe
Dave <dave@Daves-MacBook-Pro.local>
2026-03-13 23:36:31 +0100

    fix: wrong folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ const base = process.env.VITE_PUBLIC_BASE_PATH?.trim() || '/'
 
 export default defineConfig({
   base,
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()]
 })
